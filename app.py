ada4c784c23eda76665bbbdcbb518cce20a8a078
KIM JEONGSIK <wpwkdtlr@gmail.com>
2021-01-05 02:22:00 +0900

    first

--- app.py
+++ app.py
@@ -11,17 +11,18 @@ def home():
     return render_template('index.html')
 
 
-@app.route('/memo', methods=['POST'])
-def post_article():
-    # 1. 클라이언트로부터 데이터를 받기
-    # 2. meta tag를 스크래핑하기
-    # 3. mongoDB에 데이터 넣기
-    return jsonify({'result': 'success', 'msg': 'POST 연결되었습니다!'})
+# @app.route('/memo', methods=['POST'])
+# def post_article():
+#     # 1. 클라이언트로부터 데이터를 받기
+#     # 2. meta tag를 스크래핑하기
+#     # 3. mongoDB에 데이터 넣기
+#     return jsonify({'result': 'success', 'msg': 'POST 연결되었습니다!'})
+#
 
-
-@app.route('/memo', methods=['GET'])
+@app.route('/book', methods=['GET'])
 def read_articles():
     # 1. mongoDB에서 _id 값을 제외한 모든 데이터 조회해오기(Read)
+    info = list(scholarly({},{'_id: False'}))
     # 2. articles라는 키 값으로 articles 정보 보내주기
     return jsonify({'result': 'success', 'msg': 'GET 연결되었습니다!'})
 
